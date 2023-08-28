5d56dea288ac8391ef8e9c2ab221ab7d68f8dd74
Nisha Singhal <nishasinghal979@gmail.com>
2023-08-28 11:27:12 +0530

    Add login function

--- routes/user.js
+++ routes/user.js
@@ -56,4 +56,55 @@ const createUserHandler = async (req, res) => {
 // Route 1 for creating a user: no login required
 router.post('/createuser', createUserValidator, createUserHandler);
 
+
+// login validation
+const loginUserValidator = [
+  body('email', 'Enter a valid email').isEmail(), //Validate email
+  body('password', 'Password cannot be blank').exists(), //Password must exist
+];
+
+// Handler for logging in already existing user
+const loginUserHandler = async (req, res) => {
+
+
+  // Validate user input
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() }); // Bad request with validation errors
+  }
+
+  try {
+    // extract email and password from user request
+    const { email, password } = req.body;
+    // Check if user with the given email exists or not
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials. Try again!" });
+    }
+
+    // Check if the password matches the user email address 
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(401).json({ error: "Invalid credentials. Try again!" });
+    }
+
+
+    // Generate JWT token
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authtoken = jwt.sign(data, JWT_SECRET, { expiresIn: '30d' }); // Token expires in 30 days
+
+    res.json({ authtoken }); // Respond with the JWT token
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error"); 
+  }
+};
+
+// Route 2 for logging in the user: no login required
+router.post('/loginuser', loginUserValidator, loginUserHandler);
+
 module.exports = router;
